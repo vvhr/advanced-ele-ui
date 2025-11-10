197ebf49e18861aa71e3e3f04351d6ad89cf7bfd
wanghaoran <wanghaoran@qq.com>
2025-11-10 15:12:20 +0800

    feat: 导出 Icon 组件的类型定义
    - 新增 IconProps 类型接口
    - 在 Icon 组件中导出类型定义
    - 在主入口文件中统
    - 导出 Icon 类型
    - 更新版本号至 0.0.5

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,3 +1,4 @@
 import Icon from './src/Icon.vue'
 
 export { Icon }
+export type { IconProps } from './src/types'
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,56 @@ import { Icon } from './components/Icon'
 import { ZwTable } from './components/Table'
 import { Editor } from './components/Editor'
 import { Upload } from './components/Upload'
-// 导出类型
+
+// ============ 导出全局类型 ============
 export * from './types'
 
-// 导出组件
+// ============ 导出组件 ============
 export { ZwForm, Icon as ZwIcon, ZwTable, Editor as ZwEditor, Upload as ZwUpload }
+
+// ============ 导出组件类型（推荐使用命名空间方式） ============
+// Form 组件类型
+export type * as FormTypes from './components/Form/src/types'
+export type { FormProps as ZwFormProps } from './components/Form/src/types'
+
+// Table 组件类型
+export type {
+  ZwTableDefineProps,
+  ZwTableInstance,
+  ZwTablePagination,
+  ZwTableColumn,
+  ZwTableColumnFn,
+  ZwTableColumnType,
+  ZwTableColumnTypeProps,
+  ZwTableAction,
+  ZwTableEmits,
+  ZwTableProps
+} from './components/Table'
+
+// Icon 组件类型
+export type { IconProps } from './components/Icon'
+
+// Editor 组件类型
+export type {
+  EditorProps,
+  EditorEmits,
+  ToolbarKey,
+  AddToolItem,
+  AddToolItemPosition
+} from './components/Editor'
+export type { AiEditor } from 'aieditor'
+
+// Upload 组件类型
+export type {
+  UploadProps,
+  UploadFile,
+  UploadRawFile,
+  FileKeys,
+  FileTemplate
+} from './components/Upload'
+
+// ============ 导出常量 ============
+export { SIMPLE_TOOLBAR_KEYS, FULL_TOOLBAR_KEYS } from './components/Editor'
 
 // 安装函数
 const install = (app: App) => {
@@ -25,5 +70,5 @@ const install = (app: App) => {
 // 默认导出
 export default {
   install,
-  version: '0.0.1'
+  version: '0.0.5'
 }
