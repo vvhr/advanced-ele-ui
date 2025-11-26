b12cd1e40d7044ce6afa81d7461e2c69bc3dd155
wanghaoran <wanghaoran@qq.com>
2025-11-26 12:39:52 +0800

    fix(demo): The example site has been using aeui.vvhrdesign.com, so change the root path of the example site to /

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -9,38 +9,38 @@ import { resolve } from 'path'
 
 // 用于构建示例网站的配置
 export default defineConfig({
-    plugins: [
-        UnoCSS(),
-        vue(),
-        vueJsx({
-            transformOn: true,
-            mergeProps: false
-        }),
-        AutoImport({
-            imports: ['vue', '@vueuse/core'],
-            resolvers: [ElementPlusResolver()],
-            dts: 'src/types/auto-imports.d.ts',
-            eslintrc: {
-                enabled: true,
-                filepath: '.eslintrc-auto-import.json',
-                globalsPropValue: true
-            }
-        }),
-        Components({
-            dirs: [],
-            resolvers: [ElementPlusResolver()],
-            dts: 'src/types/components.d.ts'
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-            '~': resolve(__dirname, '.')
-        }
-    },
-    base: '/advanced-ele-ui/',
-    build: {
-        outDir: 'dist-demo',
-        emptyOutDir: true
+  plugins: [
+    UnoCSS(),
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: false
+    }),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/types/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+        filepath: '.eslintrc-auto-import.json',
+        globalsPropValue: true
+      }
+    }),
+    Components({
+      dirs: [],
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/types/components.d.ts'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '~': resolve(__dirname, '.')
     }
+  },
+  base: '/',
+  build: {
+    outDir: 'dist-demo',
+    emptyOutDir: true
+  }
 })
